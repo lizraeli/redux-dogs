308e9ea26230d8e0eb298d72255de0cf8eb73458
lizraeli <leo2002b@yahoo.com>
2018-01-19 02:27:18 -0500

    remove unneeded containers

--- src/components/Dogs/index.js
+++ src/components/Dogs/index.js
@@ -6,16 +6,16 @@ import { RandomDog, DogBreeds, Favorites } from "../../containers/Dogs";
 class App extends React.Component {
   renderRandomDog = props => {
     const { breed, sub } = props.match.params;
+    console.log("Render random dog. breed: ", breed, "subbreed: ", sub);
     return <RandomDog breed={breed} subBreed={sub} />;
   };
 
   render() {
     return (
       <Switch>
-        <Route exact path="/dogs/random" component={this.renderRandomDog} />
         <Route exact path="/dogs/breeds" component={DogBreeds} />
         <Route exact path="/dogs/favorites" component={Favorites} />
-
+        <Route exact path="/dogs/random" component={this.renderRandomDog} />
         <Route path="/dogs/random/:breed/:sub" render={this.renderRandomDog} />
         <Route path="/dogs/random/:breed" render={this.renderRandomDog} />
       </Switch>
--- src/containers/Dogs/index.js
+++ src/containers/Dogs/index.js
@@ -1,14 +1,5 @@
-
 import RandomDog from "./RandomDog";
-import RandomDogWithBreed from "./RandomDogWithBreed";
-import RandomDogWithBreedAndSub from "./RandomDogWithBreedAndSub";
 import DogBreeds from "./DogBreeds";
 import Favorites from "./Favorites";
 
-export {
-  RandomDog,
-  RandomDogWithBreed,
-  RandomDogWithBreedAndSub,
-  DogBreeds,
-  Favorites
-};
+export { RandomDog, DogBreeds, Favorites };
